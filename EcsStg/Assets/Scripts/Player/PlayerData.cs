d96e233f9c42c1622efa3dba291ae6c108bf2fa3
KobayashiKeisuke <keisuke.kobayashi@mixi.co.jp>
2019-06-14 19:04:35 +0900

    プレイヤー判定のためのデータを構築

---
+++ EcsStg/Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace GAME.DATA
+{
+    /// <summary>
+    /// 毎フレーム回転するオブジェクトのデータ
+    /// </summary>
+    [Serializable]
+    public struct PlayerData : IComponentData
+    {
+        /// <summary> 残機 </summary>
+        public int Life;
+        /// <summary> ボムの数 </summary>
+        public int BombCount;
+        /// <summary> アタックレベル </summary>
+        public int AtkLevel;
+        /// <summary> 移動速度レベル </summary>
+        public int SpeedLevel;
+    }
+}
